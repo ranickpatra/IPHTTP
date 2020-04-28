b3879a27580f693c3d6f9d34694ccc027ca42b32
ranickpatra <ranickpatra.nalikul@gmail.com>
2020-04-28 12:41:49 +0530

    added fileManager

--- file_operations/index.js
+++ file_operations/index.js
@@ -1,8 +1,10 @@
 const saveData = require('./saveData');
+const fileManager = require('./fileManager');
 
 
 
 // export all modules
 module.exports = {
     saveData: saveData,
+    fileManager: fileManager
 }
