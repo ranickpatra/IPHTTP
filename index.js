30d76c091f7a2a99a268b24b49fd284205720fdb
ranickpatra <ranickpatra.nalikul@gmail.com>
2020-04-29 20:51:23 +0530

    exporess server just implemented

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const file_operations = require('./file_operations/');
 const dataManager = require('./dataManager');
 const Stream = require('stream');
 const http = require('http');
+const server = require('./server');
 
 
-dataManager.addData.add_file('./test.file');
+// dataManager.addData.add_file('./teamviewer_15.5.3_amd64.deb');
 
 
-http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'img/jpg' })
+
+server.server.startServer(8080, "Server started at port 8080.....", (res, content_hash, file_name) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
     // cretae a readable stream
-    const readableStream = new Stream.Readable({read(size) {}});
+    const readableStream = new Stream.Readable({ read(size) { } });
     // pipe it to response
     readableStream.pipe(res);
     // read required data
-    dataManager.getData.read_file(
-        '4ce9558906b9ec9c8271154023a090d3d048a61818486f5d2c7b1985e3f28a80',
-        readableStream);
-    
-}).listen(8080);
+    dataManager.getData.read_file(content_hash, readableStream);
 
+});
